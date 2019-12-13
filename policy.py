447c1b6ecf2502b5e3a35efd0d0537644e0b68dd
Arnold DOMAYA <arnold.domaya@orange.com>
2019-12-13 16:48:10 +0100

    initial save

---
+++ policy.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from tensorflow.python.keras.layers import Embedding
+from tensorflow.contrib.rnn import LSTMCell, MultiRNNCell
+import itertools
+
+
+import config
+
+
+tf.enable_eager_execution()
+
+
+class Policy:
+    """
+    Our policy. Takes as input the current state as a list of
+    input/feedback indices and outputs the action distribution.
+    """
+
+    def __init__(self):
+        self.guess_embedding = Embedding(config.max_guesses + 1, 
+                                         config.guess_embedding_size)
+        self.feedback_embedding = Embedding(config.max_feedback + 1,
+                                            config.feedback_embedding_size)
+        self.lstm = MultiRNNCell([
+            LSTMCell(config.lstm_hidden_size), 
+            LSTMCell(config.lstm_hidden_size)
+        ])
+
+        self.dense = tf.layers.Dense(config.max_guesses)
+
+    @property
+    def variables(self):
+        """Return all the trainable parameters"""
+        return [
+            *self.guess_embedding.variables,
+            *self.feedback_embedding.variables,
+            *self.lstm.variables,
+            *self.dense.variables
+        ]
+
+    @property
+    def named_variables(self):
+        """Method to ensure variables across different 'Policy'
+        instances are named consistently"""
+        return dict(zip(map(str, itertools.count()), self.variables))
+
+
+    def __call__(self, game_state, with_softmax=True):
+        """
+        Do a forward pass to get the action distribution
+        """
+
+        state = self.lstm.zero_state(1, tf.float32)
+
+        for guess, feedback in game_state:
+            guess_tensor = tf.reshape(tf.convert_to_tensor(guess), (1,))
+            feedback_tensor = tf.reshape(tf.convert_to_tensor(feedback), (1,))
+            guess_embedded = self.guess_embedding(guess_tensor)
+            feedback_embedded = self.feedback_embedding(feedback_tensor)
+
+            combined_embedded = tf.concat([guess_embedded,
+                                            feedback_embedded],
+                                            axis=-1)
+            # can I do multiple inputs to the LSTM instead of concatenating?
+
+            output, state = self.lstm(combined_embedded, state)
+
+        logits = self.dense(output)
+        if with_softmax:
+            return tf.nn.softmax(logits)
+        return logits
+
+
+if __name__ == "__main__":
+    from episode import Episode
+    import numpy as np
+    np.random.seed(123)
+    p = Policy()
+    print("lstm")
+    print(p.lstm)
+    # e = Episode(p, "0000")
+    # x = p(e.generate())
+    # print(x.numpy())
+
---
+++ train.py
@@ -0,0 +1,95 @@
+import tensorflow.contrib.eager as tfe
+import tensorflow as tf
+import random
+import argparse
+import os
+import sys
+
+from episode import Episode
+from policy import Policy
+import config
+
+
+
+def train(num_episodes=1000, 
+          save_every=100, 
+          checkpoint_dir="checkpoints",
+          tensorboard_dir="tensorboard",
+          tboard_every=10):
+    pol = Policy()
+    writer = tf.contrib.summary.create_file_writer(tensorboard_dir)
+    for j in range(1, num_episodes+1):
+        random_secret = random.randint(0, config.max_guesses - 1)
+        e = Episode(pol, random_secret)
+        history = e.generate()
+
+        print("Episode length: {}".format(len(history)))
+
+        G = -1 
+
+        optimizer = \
+            tf.train.GradientDescentOptimizer(
+                learning_rate=config.reinforce_alpha*G)
+
+        for i in reversed(range(1, len(history))):
+            history_so_far = history[:i]
+            next_action, _ = history[i]
+            with tfe.GradientTape() as tape:
+                action_logits = pol(history_so_far, with_softmax=False)
+                loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+                    labels=tf.one_hot(
+                        tf.convert_to_tensor([next_action]),
+                        config.max_guesses),
+                    logits=action_logits
+                )
+
+            grads = tape.gradient(loss, pol.variables)
+            optimizer.apply_gradients(zip(grads, pol.variables))
+
+            G -= 1
+            optimizer._learning_rate = G * config.reinforce_alpha
+            optimizer._learning_rate_tensor = None
+            # hack. Should be able to pass a callable as learning_rate, see
+            # https://www.tensorflow.org/api_docs/python/tf/train/GradientDescentOptimizer#args
+            # can I perhaps submit a PR to fix this bug?
+
+            sys.stdout.write("{}/{}\r".format(len(history)-i, len(history)))
+
+        if j % save_every == 0 or j == num_episodes:
+            saver = tfe.Saver(pol.named_variables)
+            save_path = os.path.join(checkpoint_dir, 
+                                     "episode{}".format(
+                                         str(j).zfill(len(str(num_episodes)))))
+            saver.save(save_path)
+
+        if j % tboard_every == 0:
+            with writer.as_default():
+                with tf.contrib.summary.always_record_summaries():
+                    tf.contrib.summary.scalar('total_return', 
+                                              tf.convert_to_tensor([G]), 
+                                              step=j)
+    print("print of pol")
+    print(pol)
+    return pol
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train the mastermind model "
+                                     "Using the REINFORCE policy gradient "
+                                     "method")
+    parser.add_argument("--num_episodes", 
+                        help="Number of episodes to use for training",
+                        type=int, default=1000)
+    parser.add_argument("--save_every", type=int, default=100,
+                        help="Checkpoint every N episodes")
+    parser.add_argument("--checkpoint_dir", help="Checkpoint directory",
+                        default="checkpoints")
+    parser.add_argument("--board_dir", help="Tensorboard directory",
+                        default="tensorboard")
+    parser.add_argument("--tboard_every", type=int, default=10,
+                        help="Write to tensorboard every N episodes")
+    args = parser.parse_args()
+                                  
+    train(args.num_episodes, args.save_every, args.checkpoint_dir,
+          args.board_dir, args.tboard_every)
+
